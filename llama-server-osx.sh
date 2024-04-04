3cd54383413e23112e0faa1ccfd64026d88d9404
Kai Zau <kaizau@users.noreply.github.com>
2024-04-04 16:10:21 +0800

    Support multiple model dirs and sandboxing

--- llama-server-osx.sh
+++ llama-server-osx.sh
@@ -18,10 +18,11 @@ if [ "$1" == "Create ~/.config/llamaserver" ]; then
   app_dir=$(dirname "$(readlink -f "$0")")
   cp "$app_dir/options.sh" "$config_dir"
   cp "$app_dir/example.model.sh" "$config_dir"
-  open "$HOME/.config/llamaserver/"
+  cp "$app_dir/example.sandbox.sb" "$config_dir"
+  open "$config_dir"
   exit 0
 elif [ "$1" == "Edit ~/.config/llamaserver" ]; then
-  open "$HOME/.config/llamaserver/"
+  open "$config_dir"
   exit 0
 fi
 
@@ -32,38 +33,75 @@ fi
 
 source "$config_options"
 
-if [[ "$LLAMA_SERVER_SCRIPT" == *"/path/to/llama.cpp/server" ]] ||
-   [[ "$LLAMA_SERVER_MODELS" == *"/path/to/ggufs" ]]; then
+if [[ "$LLAMA_SERVER_SCRIPT" == *"/path/to/llama.cpp/server" ]] || [[ ${#LLAMA_SERVER_MODELS[@]} -eq 0 ]]; then
   echo "Edit ~/.config/llamaserver"
   exit 0
 fi
+
+#
+# Models
+#
+
+all_models=()
+
+shopt -s nullglob
+for dir in "${LLAMA_SERVER_MODELS[@]}"; do
+  for file in "$dir"/*.{gguf,model.sh}; do
+    all_models+=("$file")
+  done
+done
+shopt -u nullglob
+
+if [ ${#all_models[@]} -eq 0 ]; then
+  echo "Edit ~/.config/llamaserver"
+  exit 0
+fi
+
+model_resolve() {
+  base=$1
+  for file in "${all_models[@]}"; do
+    if [ "$(basename "$file")" == "$base" ]; then
+      echo "$file"
+      return
+    fi
+  done
+}
+
+model_menu() {
+  for file in "${all_models[@]}"; do
+    echo "$(basename "$file")"
+  done | sort -V | paste -sd "|" -
+}
 
 #
 # Commands
 #
 
 log_file="$config_dir/server.log"
+server_options=("${LLAMA_SERVER_OPTIONS[@]}");
+
+start_server() {
+  if [ -f "$LLAMA_SERVER_SANDBOX" ]; then
+    echo "sandbox-exec -f $LLAMA_SERVER_SANDBOX nohup $LLAMA_SERVER_SCRIPT ${server_options[@]}" >> "$log_file"
+    sandbox-exec -f "$LLAMA_SERVER_SANDBOX" nohup "$LLAMA_SERVER_SCRIPT" "${server_options[@]}" >> "$log_file" 2>&1 &
+  else
+    echo "nohup $LLAMA_SERVER_SCRIPT ${server_options[@]}" >> "$log_file"
+    nohup "$LLAMA_SERVER_SCRIPT" "${server_options[@]}" >> "$log_file" 2>&1 &
+  fi
+}
 
 # Command: Select model
 if [[ "$1" == *.gguf ]]; then
   echo "$1" > "$log_file"
-
-  nohup "$LLAMA_SERVER_SCRIPT" \
-    "${LLAMA_SERVER_OPTIONS[@]}" \
-    --model "$LLAMA_SERVER_MODELS/$1" \
-    --log-format text >> "$log_file" 2>&1 &
+  server_options+=("--model $(model_resolve "$1")")
+  start_server
 
 # Command: Select configured model
 elif [[ "$1" == *.model.sh ]]; then
   echo "$1" > "$log_file"
-
-  source "$LLAMA_SERVER_MODELS/$1"
-  echo "${LLAMA_SERVER_MODEL_OPTIONS[@]}" >> "$log_file"
-
-  nohup "$LLAMA_SERVER_SCRIPT" \
-    "${LLAMA_SERVER_OPTIONS[@]}" \
-    "${LLAMA_SERVER_MODEL_OPTIONS[@]}" \
-    --log-format text >> "$log_file" 2>&1 &
+  source $(model_resolve "$1")
+  server_options+=("${LLAMA_SERVER_MODEL_OPTIONS[@]}")
+  start_server
 
 # Command: Stop server
 elif [ "$1" == "Stop server" ]; then
@@ -81,20 +119,14 @@ if pgrep -f "$LLAMA_SERVER_SCRIPT" > /dev/null; then
   echo "Stop server"
   echo "Edit ~/.config/llamaserver"
   echo "----"
-  echo "DISABLED|Using: $(head -n 1 "$log_file")"
-  echo "----"
-  tail -n 5 "$log_file" | tr -s ' ' | fold -w 50 | sed 's/^/DISABLED|/'
+  if [ -f "$log_file" ]; then
+    echo "DISABLED|Using: $(head -n 1 "$log_file")"
+    echo "----"
+    tail -n 5 "$log_file" | tr -s ' ' | fold -w 50 | sed 's/^/DISABLED|/'
+  fi
 
 # Or not running
 else
-  model_menu() {
-    shopt -s nullglob
-    for file in "$LLAMA_SERVER_MODELS"/*.{gguf,model.sh}; do
-      echo "$(basename "$file")"
-    done | sort -V | paste -sd "|" -
-    shopt -u nullglob
-  }
-
   echo "STATUSTITLE|🦙"
   echo "SUBMENU|Load model|$(model_menu)"
   echo "Edit ~/.config/llamaserver"
--- options.sh
+++ options.sh
@@ -1,15 +1,29 @@
+# - Use $HOME instead of ~/, since all usage of this variable is quoted
+
 # The `server` script at the root of your llama.cpp repo
-LLAMA_SERVER_SCRIPT="~/path/to/llama.cpp/server"
+LLAMA_SERVER_SCRIPT="$HOME/path/to/llama.cpp/server"
 
 # All *.gguf and *.model.sh files will be displayed in a submenu
-LLAMA_SERVER_MODELS="~/path/to/models"
-
-# Server options (except --model)
-# https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md
-LLAMA_SERVER_OPTIONS=(
-  --host 127.0.0.1
-  --port 8080
-  --ctx-size 4096
-  --n-gpu-layers 80
-  --mlock
+LLAMA_SERVER_MODELS=(
+  "$HOME/path/to/models"
+  "$HOME/path/to/more/models"
 )
+
+# Optional server options
+# - https://github.com/ggerganov/llama.cpp/blob/master/examples/server/README.md
+# - Avoid --model, since it's set by your menu bar selection
+#LLAMA_SERVER_OPTIONS=(
+#  --host 127.0.0.1
+#  --port 8080
+#  --ctx-size 4096
+#  --n-predict 2048
+#  --n-gpu-layers 100
+#  --mlock
+#  --log-format text
+#)
+
+# Optional MacOS sandbox config
+# - https://www.karltarvas.com/macos-app-sandboxing-via-sandbox-exec.html
+# - https://mybyways.com/blog/run-code-in-a-macos-sandbox
+# - https://reverse.put.as/wp-content/uploads/2011/09/Apple-Sandbox-Guide-v1.0.pdf
+#LLAMA_SERVER_SANDBOX="$HOME/path/to/sandbox/config"
